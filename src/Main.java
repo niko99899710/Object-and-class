a187493e728ed68977a2da57e96108dbbbf7935c
Karina <bagdasarova-k@inbox.ru>
2023-03-25 00:39:02 +0500

    HW12 Все исправила

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,10 @@ public class Main {
 
     public static void task_1_3_4_5() {
         System.out.println("Задание 1-3-4-5");
-        Book book = new Book("The glass throne","Sarah J. Maas", 2012);
+        Author author = new Author("Sarah", "J. Maas");
+        Book book = new Book("The glass throne", author, 2012);
         System.out.print("Название книги: " + book.getName() + ", ");
-        System.out.print(" Aвтор: " + book.getAuthor());
+        System.out.print(" Aвтор: " + book.getAuthor().getName());
         System.out.print(" год: " + book.age + ". " );
         book.setAge(2020);
         System.out.print(" (год: " + book.age + "). " );
@@ -27,17 +28,19 @@ public class Main {
 
     public static void task_6_7() {
         System.out.println("Задание 6-7");
-        Mimi mysticism = new Mimi ("Shadow Clan", "Erin Farron", 2023);
+        Author author = new Author("Erin","Farron");
+        Mimi mysticism = new Mimi ("Shadow Clan", author,2023);
         System.out.print("Название книги: " + mysticism.getNameBook() + ", ");
-        System.out.print(" Автор: " + mysticism.getAuthor() + ", ");
+        System.out.print(" Автор: " + mysticism.getAuthor().getName() + ", ");
         System.out.print(" год: " + mysticism.ageBook + ". " );
         mysticism.setAgeBook(2022);
         System.out.print(" (год: " + mysticism.ageBook + "). " );
         System.out.println();
 
-        Mimi horror = new Mimi("Shiki", "Fuyumi");
+        Author author1 = new Author("Fuyui", "G");
+        Mimi horror = new Mimi("Shiki", author);
         System.out.print("Название книги: " + horror.getNameBook() + ", ");
-        System.out.print(" Автор: " + horror.getAuthor() + ". ");
+        System.out.print(" Автор: " + horror.getAuthor().getName() + ". ");
         System.out.println();
     }
 }
